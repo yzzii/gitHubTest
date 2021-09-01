d132aab7394c40a1d59a22bf67c8191c0a227b18
yzzii <misuprk@gmail.com>
2021-09-01 17:54:08 +0900

    layerPopUpWorking

--- common/js/Resource.js
+++ common/js/Resource.js
@@ -41,6 +41,29 @@ $('iframe').ready(function(){
 });
 
 
+
+
+// @7 레이어 팝업 브라우저 중앙에 띄우기
+$("#popOpenBtn").click(function(event){  //팝업 Open 버튼 클릭 시  
+  $("#popWarp").css({
+    "top": (($(window).height()-$("#popWarp").outerHeight())/2+$(window).scrollTop())+"px",
+    "left": (($(window).width()-$("#popWarp").outerWidth())/2+$(window).scrollLeft())+"px"
+    //팝업창을 가운데로 띄우기 위해 현재 화면의 가운데 값과 스크롤 값을 계산하여 팝업창 CSS 설정  
+  }); 
+
+  $("#popup_mask").css("display","block"); //팝업 뒷배경 display block
+  $("#popWarp").css("display","block"); //팝업창 display block 
+  $("body").css("overflow","hidden");//body 스크롤바 없애기
+  });
+
+  $("#popCloseBtn").click(function(event){
+  $("#popup_mask").css("display","none"); //팝업창 뒷배경 display none
+  $("#popWarp").css("display","none"); //팝업창 display none
+  $("body").css("overflow","auto");//body 스크롤바 생성
+  $("#popWarp").draggable();
+  });
+
+
 //@4 Full Screen
 
 let fullscreen;
@@ -209,7 +232,3 @@ document.addEventListener("click", closeAllSelect);
 
 
 // grid_example
-
-
-
-
